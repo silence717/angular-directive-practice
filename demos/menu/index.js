2c86b05aa648f1af148410bb46ba6e8bc98b3b1b
silence <silence@silence.local>
2016-09-05 22:01:59 +0800

    添加menu指令

---
+++ demos/menu/index.js
@@ -0,0 +1,204 @@
+/**
+ * @author [https://github.com/silence717]
+ * @since  2016-09-05
+ */
+/**
+ * module
+ */
+(function(angular) {
+    angular
+        .module('app', ['components.menu']);
+})(window.angular);
+/**
+ * controller
+ */
+(function(angular) {
+    angular
+        .module('app')
+        .controller('AppController', AppController);
+    function AppController() {
+        var vm = this;
+        vm.menus = [
+            {
+                'id': '11',
+                'name': 'ONLY',
+                'isLeaf': '1',
+                'brand': 'only',
+                'childMenus': [
+                    {
+                        'id': '3',
+                        'name': '基础报表',
+                        'isLeaf': '0',
+                        'brand': 'only',
+                        'url': 'report.basic'
+                    },
+                    {
+                        'id': '4',
+                        'name': '工作量分析',
+                        'isLeaf': '0',
+                        'brand': 'only',
+                        'url': 'report.workload'
+                    },
+                    {
+                        'id': '3',
+                        'name': '分时接待分析',
+                        'isLeaf': '0',
+                        'brand': 'only',
+                        'url': 'report.timeAnalysis'
+                    },
+                    {
+                        'id': '4',
+                        'name': '接待压力分析',
+                        'isLeaf': '0',
+                        'brand': 'only',
+                        'url': 'report.reception'
+                    }
+                ]
+            },
+            {
+                'id': '12',
+                'name': 'JACK&JONES',
+                'brand': 'jj',
+                'isLeaf': '1',
+                'childMenus': [
+                    {
+                        'id': '3',
+                        'name': '基础报表',
+                        'isLeaf': '0',
+                        'brand': 'jj',
+                        'url': 'report.basic'
+                    },
+                    {
+                        'id': '4',
+                        'name': '工作量分析',
+                        'isLeaf': '0',
+                        'brand': 'jj',
+                        'url': 'report.workload'
+                    },
+                    {
+                        'id': '3',
+                        'name': '分时接待分析',
+                        'isLeaf': '0',
+                        'brand': 'jj',
+                        'url': 'report.timeAnalysis'
+                    },
+                    {
+                        'id': '4',
+                        'name': '接待压力分析',
+                        'isLeaf': '0',
+                        'brand': 'jj',
+                        'url': 'report.reception'
+                    }
+                ]
+            },
+            {
+                'id': '13',
+                'name': 'VERO MODA',
+                'brand': 'vm',
+                'isLeaf': '1',
+                'childMenus': [
+                    {
+                        'id': '3',
+                        'name': '基础报表',
+                        'isLeaf': '0',
+                        'brand': 'jj',
+                        'url': 'report.basic'
+                    },
+                    {
+                        'id': '4',
+                        'name': '工作量分析',
+                        'isLeaf': '0',
+                        'brand': 'jj',
+                        'url': 'report.workload'
+                    },
+                    {
+                        'id': '3',
+                        'name': '分时接待分析',
+                        'isLeaf': '0',
+                        'brand': 'jj',
+                        'url': 'report.timeAnalysis'
+                    },
+                    {
+                        'id': '4',
+                        'name': '接待压力分析',
+                        'isLeaf': '0',
+                        'brand': 'jj',
+                        'url': 'report.reception'
+                    }
+                ]
+            },
+            {
+                'id': '14',
+                'name': 'SELECTED',
+                'brand': 'slt',
+                'isLeaf': '1',
+                'childMenus': [
+                    {
+                        'id': '3',
+                        'name': '基础报表',
+                        'isLeaf': '0',
+                        'brand': 'slt',
+                        'url': 'report.basic'
+                    },
+                    {
+                        'id': '4',
+                        'name': '工作量分析',
+                        'isLeaf': '0',
+                        'brand': 'slt',
+                        'url': 'report.workload'
+                    },
+                    {
+                        'id': '3',
+                        'name': '分时接待分析',
+                        'isLeaf': '0',
+                        'brand': 'slt',
+                        'url': 'report.timeAnalysis'
+                    },
+                    {
+                        'id': '4',
+                        'name': '接待压力分析',
+                        'isLeaf': '0',
+                        'brand': 'slt',
+                        'url': 'report.reception'
+                    }
+                ]
+            },
+            {
+                'id': '15',
+                'name': 'J.LINDEBERG',
+                'brand': 'jl',
+                'isLeaf': '1',
+                'childMenus': [
+                    {
+                        'id': '3',
+                        'name': '基础报表',
+                        'isLeaf': '0',
+                        'brand': 'slt',
+                        'url': 'report.basic'
+                    },
+                    {
+                        'id': '4',
+                        'name': '工作量分析',
+                        'isLeaf': '0',
+                        'brand': 'slt',
+                        'url': 'report.workload'
+                    },
+                    {
+                        'id': '3',
+                        'name': '分时接待分析',
+                        'isLeaf': '0',
+                        'brand': 'slt',
+                        'url': 'report.timeAnalysis'
+                    },
+                    {
+                        'id': '4',
+                        'name': '接待压力分析',
+                        'isLeaf': '0',
+                        'brand': 'slt',
+                        'url': 'report.reception'
+                    }
+                ]
+            }
+        ];
+    }
+})(window.angular);
